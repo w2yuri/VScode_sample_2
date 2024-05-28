29af0dfa1c6e00fdd3038327aae5e1b03920d383
w2yuri <namatro3@gmail.com>
2024-05-28 20:59:10 +0900

    edit,update追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to root
+    redirect_to list_path(list.id)
   end 
   
   def index
@@ -18,7 +18,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end 
 
   private
   def list_params
